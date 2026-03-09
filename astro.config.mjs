1bd1e859cabb9c1c821e220cb5f1963b5381b4ca
ilya <elijahmg25@gmail.com>
2026-03-09 09:47:52 +0100

    remove gig

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://tonguepunch.com', // Replace with your actual domain later
+  site: 'https://www.tonguepunchmusic.com/', // Replace with your actual domain later
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
